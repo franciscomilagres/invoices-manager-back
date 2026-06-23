1a30585a8ca48ef5028f506dbc9811532d61c72a
Francisco <francisco.lima93@yahoo.com.br>
2026-06-23 01:51:38 +0100

    first healthy with prisma, db and model

--- api/routes/health.ts
+++ api/routes/health.ts
@@ -1,9 +1,15 @@
 import { Router, Request, Response } from 'express';
+import { healthService } from '../services/health.service';
 
 const router = Router();
 
 router.get('/', (req: Request, res: Response) => {
   res.send('Hey there');
 });
+
+router.get('/db', async (req: Request, res: Response) => {
+  const result = await healthService.countTables();
+  res.status(200).json(result);
+})
 
 export default router;
---
+++ api/services/health.service.ts
@@ -0,0 +1,35 @@
+import "dotenv/config";
+import postgres from 'postgres';
+import { prisma } from "../utils/db.utils";
+
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+
+export interface ITablesCount {
+  customersCount: number;
+  invoicesCount: number;
+  paymentsCount: number;
+}
+
+export class HealthService {
+  async countTables(): Promise<ITablesCount> {
+    try {
+      
+      const customersCountPromise = prisma.im_Customer.count();
+      const invoicesCountPromise = prisma.im_Invoice.count();
+      const paymentsCountPromise = prisma.im_Payment.count();
+
+      const data = await Promise.all([customersCountPromise, invoicesCountPromise, paymentsCountPromise]);
+
+      const customersCount = Number(data[0] ?? '0');
+      const invoicesCount = Number(data[1] ?? '0');
+      const paymentsCount = Number(data[2] ?? '0');
+
+      return { customersCount, invoicesCount, paymentsCount };
+    } catch (error) {
+      console.error(error);
+      throw new Error('Failed to fetch card data');
+    }
+  }
+}
+
+export const healthService = new HealthService();
